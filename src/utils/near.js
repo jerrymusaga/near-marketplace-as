609e68ce7a9d98e9a3b925af746f4ca668c39cfd
jerrymusaga <musagatechnology@gmail.com>
2022-08-05 03:05:00 +0100

    chore:project init

---
+++ src/utils/near.js
@@ -0,0 +1,24 @@
+import environment from './config';
+import {connect,keyStores,Contract,WalletConnection} from 'near-api-js';
+import {formatNearAmount} from 'near-api-js/lib/utils/format';
+
+const nearEnv = environment("testnet");
+
+export async function initializeContract() {
+    const near = await connect(
+      Object.assign(
+        { deps: { keyStore: new keyStores.BrowserLocalStorageKeyStore() } },
+        nearEnv
+      )
+    );
+    window.walletConnection = new WalletConnection(near);
+    window.accountId = window.walletConnection.getAccountId();
+    window.contract = new Contract(
+      window.walletConnection.account(),
+      nearEnv.contractName,
+      {
+        viewMethods: ["getProduct", "getProducts"],
+        changeMethods: ["buyProduct", "setProduct"],
+      }
+    );
+}
